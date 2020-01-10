e4e5d2fde0e3ba223489e5e98b5000564ab773bb
sadanand.h <sadanand.h@quali.com>
2020-01-10 09:35:23 -0500

    Initial commit

---
+++ tests/test_linuxshell.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Tests for `LinuxshellDriver`
+"""
+
+import unittest
+
+from driver import LinuxshellDriver
+
+
+class TestLinuxshellDriver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
